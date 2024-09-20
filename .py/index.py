d42d1c502f3bd38b3b8f28fca18555f0b2d55463
sreehari <abhinavcv227@gmail.com>
2024-09-20 15:55:07 +0530

    initial commit

---
+++ .py/index.py
@@ -0,0 +1,10 @@
+
+add=a+ba=int(input("Enter a Number "))
+b=int(input("Enter a Number "))
+sub=a-b
+div=a/b
+mul=a*b
+print("A=B",add)
+print("A=B",sub)
+print("A=B",div)
+print("A=B",mul)
---
+++ .py/index1.py
@@ -0,0 +1,19 @@
+# a=int(input("Enter a Number "))
+# b=int(input("Enter a Number "))
+# if a>b:
+#     print (a,"is greater")
+# else:
+#     print (b,"is greater")
+
+
+# a=int(input("Enter a Number "))
+# b=int(input("Enter a Number "))
+# c=int(input("Enter a Number "))
+# if a>b and a>c:
+#     print(a,"is greater")
+# elif b>a and b>c:
+#     print(b,"is greater")
+# else:
+#     print(c,"is greater")
+
+
