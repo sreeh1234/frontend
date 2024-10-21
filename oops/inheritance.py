33860615d9a43bd1ce8a33711242f84487a72386
vishnup0888 <vishnup08888@gmail.com>
2024-10-21 16:27:31 +0530

    initial commit

---
+++ oops/inheritance.py
@@ -0,0 +1,42 @@
+# class syn:
+#     def python(self):
+#         print("pyhton")
+#     def php(self):
+#         print("php")    
+        
+
+# class novavi (syn):
+#     def web_dev(self):
+#         print("web dev")     
+#     def dm(self):
+#         print("dm")       
+        
+# akhil=syn()        
+# akhil.php()
+
+# staff1=novavi()
+# staff1.python()
+
+
+
+
+class zomato:
+    def view(self):
+        print("order viewed")
+    def update(self):
+        print("order updated")  
+        
+class blinkit(zomato):
+    def receive(self):
+        print("order received")
+    def deliver(self):
+        print("deliver")
+        
+        
+cus1=zomato()
+cus1.update()            
+
+cus2=blinkit()
+cus2.view()
+            
+        
---
+++ oops/methods.py
@@ -0,0 +1,35 @@
+# class method():
+#     def python():
+#         print('fullstack')
+        
+#     def php():
+#         print('3 months')    
+        
+# student=method
+# student.python()          
+# student.php()
+
+
+
+# class bank:
+#     def __init__(s,id,name,bal):
+#         s.id=id
+#         s.name=name
+#         s.bal=bal
+#     def balance(s):
+#         print('bal',s.bal)
+#     def depo(s,amt):
+#         depo_amt=amt
+#         s.bal+=amt
+        
+# cus1=bank(1,'abc',0)
+# cus1.depo(200)
+# cus1.balance()
+
+
+# cus2=bank(2,'def',0)
+# cus2.depo(400)
+# cus2.balance()
+
+
+
